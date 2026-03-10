5f8dc05a4756ea549dab3b52fabe3396cff6f2ff
i19jpilla <i19jpilla@ifieldcc.co.uk>
2026-03-10 14:50:12 +0000

    increased contrast for portfolio colors

--- frontend/pages/portfolio/index.js
+++ frontend/pages/portfolio/index.js
@@ -30,11 +30,7 @@ function updatePieChart() {
     if (pieChart) pieChart.destroy()
 
     function getRandomColor() {
-        const r = Math.floor(Math.random() * 256)
-        const g = Math.floor(Math.random() * 256)
-        const b = Math.floor(Math.random() * 256)
-
-        return `rgb(${r},${g},${b})`
+        return `hsl(${Math.floor(Math.random() * 360)}, 70%, 50%)`
     }
 
     pieChart = new Chart(canvas, {
